4c8f9b7ad7dec0480d25ef8bd86510c2bf6cbf5a
Boo Pannachai <booya.pannachai@gmail.com>
2026-01-14 21:06:35 +0700

    initial commit

---
+++ TradingTable/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TradingTable
+//
+//  Created by Panachai Sulsaksakul on 1/14/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
