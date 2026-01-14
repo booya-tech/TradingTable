0082b4cf4133a6f20eea98e72d822998f5fdb894
Boo Pannachai <booya.pannachai@gmail.com>
2026-01-14 21:28:58 +0700

    feat, chore: (feat)create stock (chore)model, update .gitignore

--- TradingTable/Models/Stock.swift
+++ TradingTable/Models/Stock.swift
@@ -6,3 +6,32 @@
 //
 
 import Foundation
+
+// Domain Model
+struct Stock {
+    let symbol: String
+    let companyName: String
+    let logoURL: String
+    let price: Double
+    let priceChange: Double
+    let priceChangePercent: Double
+}
+
+// External Models
+struct CompanyProfile: Codable {
+    let ticker: String
+    let name: String
+    let logo: String
+}
+
+struct StockQuote: Codable {
+    let currentPrice: Double
+    let change: Double
+    let percentChange: Double
+
+    enum CodingKeys: String, CodingKey {
+        case currentPrice = "c"
+        case change = "d"
+        case percentChange = "dp"
+    }
+}
